ccb6493360d70ad3ceb44439930bc9419032c9f9
Carson Bartos <1100030962@edu.cbe.ab.ca>
2022-02-16 12:18:20 -0700

    started doing stuff

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 
+//Globals
+var clicks = 0
+let money = 100
+
+
+document.getElementById("goodLuckButton").onclick = function(){click()};
+
+clickText = document.getElementById("clicksText")
+moneyText = document.getElementById("moneyText")
+moneyText.innerHTML = `Money: ${money}`
+
+
+
+function click(){
+    console.log("click")
+    clicks++
+    clickText.innerHTML = `Clicks: ${clicks}`
+    let random = Math.floor(Math.random() * 11)
+    let guess = 1;
+    console.log(guess)
+    console.log(random)
+    if(guess == random){win()}
+}
+
+
+function win(){
+    clickText.innerHTML = `Clicks: ${clicks}`
+    alert("Wow")
+    alert("You did it")
+    alert(`After ${clicks} clicks`)
+    alert("You just you just won $")
+    alert("Congrats")
+    clicks = 0
+}
+
+
+
+
+
+
